a9d2a171de38c1f1cbe234c3a14267846ca4a69a
Ohleesang <jammmmed@gmail.com>
2023-12-27 10:16:38 +0900

    간단한 문자열 예제

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,2 +1,13 @@
 class Solution {
+    fun solution(message: String): Int {
+        var answer: Int = 0
+        answer = message.length*2
+        return answer
+    }
+}
+
+fun main(){
+    val a =Solution()
+    a.solution("happy birthday!")//30
+    a.solution("I love you~")//22
 }
